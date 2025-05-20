b99d8d9229728e6cdd473ff9b89c012b580e9a01
Andrey Shishkov <shishkoffandrey@yandex.ru>
2025-05-20 12:25:30 +0300

    Add repository-classes which realize logic for db and cache

---
+++ repository/__init__.py
@@ -0,0 +1,5 @@
+from repository.task_rep import TaskRepository
+from repository.cache_task import CacheTask
+
+
+__all__ = ['TaskRepository', 'CacheTask']
---
+++ repository/task_rep.py
@@ -0,0 +1,55 @@
+from sqlalchemy.orm import Session
+from sqlalchemy import select, delete, update
+from database import Task, Category, get_db_session
+from schemas.task import TaskSchema
+
+
+class TaskRepository:
+    def __init__(self, db_session):
+        self.db_session = db_session
+
+    def get_task(self, task_id) -> Task | None:
+        query = select(Task).where(Task.id == task_id)
+        with self.db_session() as session:
+            task: Task = session.execute(query).scalar_one_or_none()
+        return  task
+
+    def get_tasks(self) -> list[Task]:
+        with self.db_session() as session:
+            tasks: list[Task] = session.execute(select(Task)).scalars().all()
+        return tasks
+
+    def create_task(self, task: TaskSchema) -> int:
+        task_db_row = Task(
+            name=task.name,
+            pomodoro_number=task.pomodoro_number,
+            category_id=task.category_id,
+        )
+        with self.db_session() as session:
+            session.add(task_db_row)
+            session.commit()
+            return task_db_row.id
+
+    def delete_task(self, task_id: int) -> None:
+        with self.db_session() as session:
+            session.execute(delete(Task).where(Task.id == task_id))
+            session.commit()
+
+    def get_tasks_by_category_name(self, category_name: str) -> list[Task]:
+        query = select(Task).join(Category, Task.category_id == Category.id)\
+        .where(Category.name == category_name)
+        with self.db_session() as session:
+            tasks: list[Task] = session.execute(query).scalars().all()
+        return tasks
+
+    def update_task_name(self, task_id: int, name: str) -> Task:
+        query = update(Task).where(Task.id == task_id).values(name=name).returning(Task.id)
+        with self.db_session() as session:
+            task_id: int = session.execute(query).scalar_one_or_none()
+            session.commit()
+            return self.get_task(task_id)
+
+
+def get_tasks_repository() -> TaskRepository:
+    db_session = get_db_session()
+    return TaskRepository(db_session)
