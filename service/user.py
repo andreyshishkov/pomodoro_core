5a81b1dd67705ab9af1768803963b09c30cd94a7
Andrey Shishkov <shishkoffandrey@yandex.ru>
2025-05-22 15:19:19 +0300

    Create skeleton for UserService

---
+++ service/user.py
@@ -0,0 +1,9 @@
+from dataclasses import dataclass
+from schemas import UserLoginSchema
+
+
+@dataclass
+class UserService:
+
+    def create_user(self, username: str, password: str) -> UserLoginSchema:
+        pass
