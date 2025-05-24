ae0118a921abffa1f6904ca83fc5edd6012d08dc
Andrey Shishkov <shishkoffandrey@yandex.ru>
2025-05-24 18:23:06 +0300

    Add handlers to login

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,9 +1,11 @@
 from handlers.tasks import task_router
 from handlers.ping import ping_router
 from handlers.user import router as user_router
+from handlers.auth import router as auth_router
 
 routers = [
     task_router,
     ping_router,
-    user_router
+    user_router,
+    auth_router,
 ]
