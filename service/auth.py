f531f0ad1a4fdd15b8f5b52f5e700b81bf97f611
Andrey Shishkov <shishkoffandrey@yandex.ru>
2025-06-01 03:57:15 +0300

    Refactor auth-service for jwt

--- service/auth.py
+++ service/auth.py
@@ -1,20 +1,27 @@
 from dataclasses import dataclass
 
+import datetime as dt
+
+from jose import jwt
+
 from models import UserProfile
 from schemas import UserLoginSchema
 
 from repository import UserRepository
 from exception import UserNotFoundException, UserNotCorrectPasswordException
+from settings import Settings
 
 
 @dataclass
 class AuthService:
     user_repository: UserRepository
+    settings: Settings
 
     def login(self, username: str, password: str) -> UserLoginSchema:
         user = self.user_repository.get_user_by_username(username)
         self._validate_auth_user(user, password)
-        return UserLoginSchema(user_id=user.id, access_token=user.access_token)
+        access_token = self.generate_access_token(user.id)
+        return UserLoginSchema(user_id=user.id, access_token=access_token)
 
     @staticmethod
     def _validate_auth_user(user: UserProfile, password: str) -> None:
@@ -22,3 +29,12 @@ class AuthService:
             raise UserNotFoundException
         if user.password != password:
             raise UserNotCorrectPasswordException
+
+    def generate_access_token(self, user_id: int) -> str:
+        expires_date_unix = (dt.datetime.now(dt.UTC) + dt.timedelta(days=7)).timestamp()
+        token = jwt.encode(
+            {'user_id': user_id, 'expire': expires_date_unix},
+            self.settings.JWT_SECRET_KEY,
+            algorithm=self.settings.JWT_ENCODE_ALGORITHM,
+        )
+        return token
