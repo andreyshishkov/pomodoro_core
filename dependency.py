884b9fe1320d7ea2ab3824d10356bdf4d72b23d5
Andrey Shishkov <shishkoffandrey@yandex.ru>
2025-06-01 03:58:55 +0300

    Refactor dependency for auth-service

--- dependency.py
+++ dependency.py
@@ -6,6 +6,7 @@ from service import TaskService, UserService
 from sqlalchemy.orm import Session
 
 from service.auth import AuthService
+from settings import Settings
 
 
 def get_task_repository() -> TaskRepository:
@@ -32,13 +33,15 @@ def get_user_repository(db_session: Session = Depends(get_db_session)) -> UserRe
     return UserRepository(db_session=db_session)
 
 
-def get_user_service(
-        user_repository: UserRepository = Depends(get_user_repository)
-) -> UserService:
-    return UserService(user_repository=user_repository)
-
-
 def get_auth_service(
         user_repository: UserRepository = Depends(get_user_repository)
 ):
-    return AuthService(user_repository=user_repository)
+    return AuthService(user_repository=user_repository, settings=Settings())
+
+
+def get_user_service(
+        user_repository: UserRepository = Depends(get_user_repository),
+        auth_service: AuthService = Depends(get_auth_service),
+) -> UserService:
+    return UserService(user_repository=user_repository, auth_service=auth_service)
+
