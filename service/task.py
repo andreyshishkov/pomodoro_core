a61c329652eec9a8b2a72829ff80c597f2edaf01
Andrey Shishkov <shishkoffandrey@yandex.ru>
2025-06-06 00:45:53 +0300

    Add new func create_task for task-service

--- service/task.py
+++ service/task.py
@@ -1,5 +1,5 @@
 from repository import TaskRepository, CacheTask
-from schemas.task import TaskSchema
+from schemas import TaskSchema, TaskCreateSchema
 
 from dataclasses import dataclass
 
@@ -16,3 +16,8 @@ class TaskService:
         tasks_schema = [TaskSchema.model_validate(task) for task in tasks]
         self.task_cache.set_tasks(tasks_schema)
         return tasks_schema
+
+    def create_task(self, body: TaskCreateSchema, user_id: int) -> TaskSchema:
+        task_id = self.task_repository.create_task(body, user_id)
+        task = self.task_repository.get_task(task_id)
+        return TaskSchema.model_validate(task)
