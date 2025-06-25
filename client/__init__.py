5969edaecd0fb63535168c9e48e8047a816c374c
Andrey Shishkov <shishkoffandrey@yandex.ru>
2025-06-25 07:14:30 +0300

    create:yandex-client

--- client/__init__.py
+++ client/__init__.py
@@ -1,4 +1,8 @@
 from client.google_client import GoogleClient
+from client.yandex_client import YandexClient
 
 
-__all__ = ['GoogleClient']
+__all__ = [
+    'GoogleClient',
+    'YandexClient',
+]
