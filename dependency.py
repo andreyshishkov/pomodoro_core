6ca0d658ac1c7bed8bf647a60ed482629a93c903
Andrey Shishkov <shishkoffandrey@yandex.ru>
2025-06-25 07:26:50 +0300

    Add yandex-client dependency

--- dependency.py
+++ dependency.py
@@ -1,6 +1,6 @@
 from fastapi import Depends, security, Security, HTTPException
 
-from client import GoogleClient
+from client import GoogleClient, YandexClient
 from database import get_db_session
 from repository import TaskRepository, CacheTask, UserRepository
 from cache import get_redis_connection
@@ -40,14 +40,20 @@ def get_google_client() -> GoogleClient:
     return GoogleClient(settings=Settings())
 
 
+def get_yandex_client() -> YandexClient:
+    return YandexClient(settings=Settings())
+
+
 def get_auth_service(
         user_repository: UserRepository = Depends(get_user_repository),
-        google_client: GoogleClient = Depends(get_google_client)
+        google_client: GoogleClient = Depends(get_google_client),
+        yandex_client: YandexClient = Depends(get_yandex_client)
 ):
     return AuthService(
         user_repository=user_repository,
         settings=Settings(),
         google_client=google_client,
+        yandex_client=yandex_client,
     )
 
 
