f89fc416cb814ac31c4560a005ca22478a6d27f4
Andrey Shishkov <shishkoffandrey@yandex.ru>
2025-06-27 01:36:00 +0300

    refactor: redis to asyncronic

--- service/task.py
+++ service/task.py
@@ -10,12 +10,12 @@ class TaskService:
     task_repository: TaskRepository
     task_cache: CacheTask
 
-    def get_tasks(self):
-        if tasks := self.task_cache.get_tasks():
+    async def get_tasks(self):
+        if tasks := await self.task_cache.get_tasks():
             return tasks
         tasks = self.task_repository.get_tasks()
         tasks_schema = [TaskSchema.model_validate(task) for task in tasks]
-        self.task_cache.set_tasks(tasks_schema)
+        await self.task_cache.set_tasks(tasks_schema)
         return tasks_schema
 
     def create_task(self, body: TaskCreateSchema, user_id: int) -> TaskSchema:
