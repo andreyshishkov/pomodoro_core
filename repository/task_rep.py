ee016156ace0ff7291d98e2f4258cf8235ada69f
Andrey Shishkov <shishkoffandrey@yandex.ru>
2025-06-29 01:19:24 +0300

    refctor: task-repository to async

--- repository/task_rep.py
+++ repository/task_rep.py
@@ -1,63 +1,57 @@
-from sqlalchemy.orm import Session
+from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import select, delete, update
-from database import get_db_session
 from models import Task, Category
-from schemas import TaskSchema, TaskCreateSchema
+from schemas import TaskCreateSchema
 
 
 class TaskRepository:
-    def __init__(self, db_session):
+    def __init__(self, db_session: AsyncSession):
         self.db_session = db_session
 
-    def get_task(self, task_id) -> Task | None:
+    async def get_task(self, task_id) -> Task | None:
         query = select(Task).where(Task.id == task_id)
-        with self.db_session() as session:
-            task: Task = session.execute(query).scalar_one_or_none()
+        async with self.db_session as session:
+            task: Task = (await session.execute(query)).scalar_one_or_none()
         return task
 
-    def get_user_task(self, user_id: int, task_id: int) -> Task | None:
+    async def get_user_task(self, user_id: int, task_id: int) -> Task | None:
         query = select(Task).where(Task.id == task_id, Task.user_id == user_id)
-        with self.db_session() as session:
-            task: Task = session.execute(query).scalar_one_or_none()
+        async with self.db_session as session:
+            task: Task = (await session.execute(query)).scalar_one_or_none()
         return task
 
-    def get_tasks(self) -> list[Task]:
-        with self.db_session() as session:
-            tasks: list[Task] = session.execute(select(Task)).scalars().all()
+    async def get_tasks(self) -> list[Task]:
+        async with self.db_session as session:
+            tasks: list[Task] = (await session.execute(select(Task))).scalars().all()
         return tasks
 
-    def create_task(self, task: TaskCreateSchema, user_id: int) -> int:
+    async def create_task(self, task: TaskCreateSchema, user_id: int) -> int:
         task_db_row = Task(
             name=task.name,
             pomodoro_number=task.pomodoro_number,
             category_id=task.category_id,
             user_id=user_id,
         )
-        with self.db_session() as session:
+        async with self.db_session as session:
             session.add(task_db_row)
-            session.commit()
+            await session.commit()
             return task_db_row.id
 
-    def delete_task(self, task_id: int, user_id: int) -> None:
-        with self.db_session() as session:
-            session.execute(delete(Task).where(Task.id == task_id, Task.user_id == user_id))
-            session.commit()
+    async def delete_task(self, task_id: int, user_id: int) -> None:
+        async with self.db_session as session:
+            await session.execute(delete(Task).where(Task.id == task_id, Task.user_id == user_id))
+            await session.commit()
 
-    def get_tasks_by_category_name(self, category_name: str) -> list[Task]:
+    async def get_tasks_by_category_name(self, category_name: str) -> list[Task]:
         query = select(Task).join(Category, Task.category_id == Category.id)\
         .where(Category.name == category_name)
-        with self.db_session() as session:
-            tasks: list[Task] = session.execute(query).scalars().all()
+        async with self.db_session as session:
+            tasks: list[Task] = (await session.execute(query)).scalars().all()
         return tasks
 
-    def update_task_name(self, task_id: int, name: str) -> Task:
+    async def update_task_name(self, task_id: int, name: str) -> Task:
         query = update(Task).where(Task.id == task_id).values(name=name).returning(Task.id)
-        with self.db_session() as session:
-            task_id: int = session.execute(query).scalar_one_or_none()
-            session.commit()
-            return self.get_task(task_id)
-
-
-def get_tasks_repository() -> TaskRepository:
-    db_session = get_db_session()
-    return TaskRepository(db_session)
+        async with self.db_session as session:
+            task_id: int = (await session.execute(query)).scalar_one_or_none()
+            await session.commit()
+            return await self.get_task(task_id)
