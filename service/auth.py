bcb91300c9c0edd303099b2de0c00d682df29f5e
Andrey Shishkov <shishkoffandrey@yandex.ru>
2025-06-29 01:56:19 +0300

    refactor: auth-service

--- service/auth.py
+++ service/auth.py
@@ -21,8 +21,8 @@ class AuthService:
     google_client: GoogleClient
     yandex_client: YandexClient
 
-    def login(self, username: str, password: str) -> UserLoginSchema:
-        user = self.user_repository.get_user_by_username(username)
+    async def login(self, username: str, password: str) -> UserLoginSchema:
+        user = await self.user_repository.get_user_by_username(username)
         self._validate_auth_user(user, password)
         access_token = self.generate_access_token(user.id)
         return UserLoginSchema(user_id=user.id, access_token=access_token)
@@ -32,7 +32,7 @@ class AuthService:
 
     async def google_auth(self, code: str):
         user_data = await self.google_client.get_user_info(code)
-        if user := self.user_repository.get_user_by_email(email=user_data.email):
+        if user := await self.user_repository.get_user_by_email(email=user_data.email):
             access_token = self.generate_access_token(user_id=user.id)
             return UserLoginSchema(user_id=user.id, access_token=access_token)
 
@@ -40,7 +40,7 @@ class AuthService:
             email=user_data.email,
             name=user_data.name,
         )
-        created_user = self.user_repository.create_user(create_user_data)
+        created_user = await self.user_repository.create_user(create_user_data)
         access_token = self.generate_access_token(user_id=created_user.id)
         return UserLoginSchema(user_id=user.id, access_token=access_token)
 
@@ -50,7 +50,7 @@ class AuthService:
 
     async def yandex_auth(self, code: str):
         user_data = await self.yandex_client.get_user_info(code)
-        if user := self.user_repository.get_user_by_email(email=user_data.default_email):
+        if user := await self.user_repository.get_user_by_email(email=user_data.default_email):
             access_token = self.generate_access_token(user_id=user.id)
             return UserLoginSchema(user_id=user.id, access_token=access_token)
 
@@ -58,7 +58,7 @@ class AuthService:
             email=user_data.default_email,
             name=user_data.name,
         )
-        created_user = self.user_repository.create_user(create_user_data)
+        created_user = await self.user_repository.create_user(create_user_data)
         access_token = self.generate_access_token(user_id=created_user.id)
         return UserLoginSchema(user_id=user.id, access_token=access_token)
 
