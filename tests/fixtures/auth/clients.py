80c4578a7da11fd1751d969f857c923dc07a2488
Andrey Shishkov <shishkoffandrey@yandex.ru>
2025-07-19 08:31:46 +0300

    add clients reaponse data for fixtures

--- tests/fixtures/auth/clients.py
+++ tests/fixtures/auth/clients.py
@@ -3,6 +3,11 @@ from dataclasses import dataclass
 import pytest
 
 from app.settings import Settings
+from app.users.auth.schema import GoogleUserData, YandexUserData
+
+from faker import Factory as FakerFactory
+
+faker = FakerFactory.create()
 
 
 @dataclass
@@ -10,9 +15,9 @@ class FakeGoogleClient:
     settings: Settings
     async_client: httpx.AsyncClient
 
-    async def get_user_info(self, code) -> dict:
-        access_token = self._get_user_access_token(code)
-        return {'fake_access_token': access_token}
+    async def get_user_info(self, code) -> GoogleUserData:
+        access_token = await self._get_user_access_token(code)
+        return google_user_info_data()
 
     async def _get_user_access_token(self, code: str) -> str:
         return f'fake_access_token {code}'
@@ -23,9 +28,9 @@ class FakeYandexClient:
     settings: Settings
     async_client: httpx.AsyncClient
 
-    async def get_user_info(self, code) -> dict:
-        access_token = self._get_user_access_token(code)
-        return {'fake_access_token': access_token}
+    async def get_user_info(self, code) -> YandexUserData:
+        access_token = await self._get_user_access_token(code)
+        return yandex_user_info_data()
 
     async def _get_user_access_token(self, code: str) -> str:
         return f'fake_access_token {code}'
@@ -39,3 +44,21 @@ def google_client():
 @pytest.fixture
 def yandex_client():
     return FakeYandexClient(settings=Settings(), async_client=httpx.AsyncClient())
+
+
+def google_user_info_data() -> GoogleUserData:
+    return GoogleUserData(
+        id=faker.random_int(),
+        email=faker.email(),
+        name=faker.name(),
+        verified_email=True,
+    )
+
+
+def yandex_user_info_data() -> YandexUserData:
+    return YandexUserData(
+        id=faker.random_int(),
+        default_email=faker.email(),
+        login=faker.name(),
+        real_name=faker.name(),
+    )
