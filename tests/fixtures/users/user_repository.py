b38e483f64112073c93084ff1f4808ef181bc861
Andrey Shishkov <shishkoffandrey@yandex.ru>
2025-07-19 08:38:45 +0300

    add methods for fake-user-repository

--- tests/fixtures/users/user_repository.py
+++ tests/fixtures/users/user_repository.py
@@ -1,10 +1,17 @@
 import pytest
 from dataclasses import dataclass
 
+from app.users.user_profile.schema import UserCreateSchema
+from tests.fixtures.users.user_model import UserProfileFactory
+
 
 @dataclass
 class FakeUserRepository:
-    pass
+    async def get_user_by_email(self, email: str):
+        return None
+
+    async def create_user(self, user_data: UserCreateSchema):
+        return UserProfileFactory()
 
 
 @pytest.fixture
