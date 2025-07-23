bdfb7b0e7e37ef61f3b1bc0a57c6ca0b0dc25b04
Andrey Shishkov <shishkoffandrey@yandex.ru>
2025-07-23 08:32:06 +0300

    create fixture: auth-service for integration tests

--- tests/fixtures/auth/auth_service.py
+++ tests/fixtures/auth/auth_service.py
@@ -1,6 +1,8 @@
 import pytest
+import pytest_asyncio
 from app.settings import Settings
 from app.users.auth.service import AuthService
+from app.users.user_profile.repository import UserRepository
 
 
 @pytest.fixture
@@ -11,6 +13,20 @@ def mock_auth_service(
 ):
     return AuthService(
         user_repository=fake_user_repository,
+        settings=Settings(),
+        google_client=google_client,
+        yandex_client=yandex_client,
+    )
+
+
+@pytest_asyncio.fixture
+async def auth_service(
+        google_client,
+        yandex_client,
+        get_db_session
+):
+    return AuthService(
+        user_repository=UserRepository(db_session=get_db_session),
         settings=Settings(),
         google_client=google_client,
         yandex_client=yandex_client,
